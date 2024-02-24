14c17cfbafb351206e73a69f786ddd55edc171e8
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2024-02-24 14:25:15 -0700

    add config file

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: 'http://localhost:3000',
+    clientID: 'i3IVAJ5InNjrFCjgPvzXpMl1Lhx2VK0J',
+    issuerBaseURL: 'https://dev-uzwzktxf6t5vdmkd.us.auth0.com'
+};
+
+module.exports = config;
