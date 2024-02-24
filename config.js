3d177ae12a349da67b2b5bbdc91009b152853d4c
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2024-02-24 14:43:42 -0700

    baseURL for config file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.SECRET,
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://harry-potter-72ok.onrender.com/api-docs',
     clientID: 'i3IVAJ5InNjrFCjgPvzXpMl1Lhx2VK0J',
     issuerBaseURL: 'https://dev-uzwzktxf6t5vdmkd.us.auth0.com'
 };
